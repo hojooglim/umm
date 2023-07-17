66f3a200d43fa15ac41a4bd6c005bcfdcb3ff76d
hojooglim <hojooglim@gmail.com>
2023-07-17 14:24:12 +0900

    init commit

---
+++ src/main/java/com/example/umm/UmmApplication.java
@@ -0,0 +1,13 @@
+package com.example.umm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/umm/UmmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
